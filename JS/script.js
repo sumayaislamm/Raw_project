6633671f6a5084fab386f89403113f71634eb12d
Sumaya <sheikhsumaya622@gmail.com>
2025-11-07 20:37:10 +0600

    after adding image

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,36 @@
 
 //For the home page Second section
 
+// document.addEventListener('DOMContentLoaded', () => {
+//   const container = document.getElementById('cards-container');
+
+// fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+//   .then(res => res.json())
+//   .then(data => {
+//     data.forEach(post => {
+//       const card = document.createElement('div');
+//       card.className = 'card';
+//       card.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+//       container.appendChild(card); 
+//     });
+//   })
+//   .catch(err => console.error(err));
+// });
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('cards-container');
 
-fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(post => {
-      const card = document.createElement('div');
-      card.className = 'card';
-      card.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
-      container.appendChild(card); 
-    });
-  })
-  .catch(err => console.error(err));
+  fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    .then(res => res.json())
+    .then(data => {
+      data.forEach((post, index) => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style.animationDelay = `${index * 0.2}s`; // adds delay per card
+        card.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+        container.appendChild(card);
+      });
+    })
+    .catch(err => console.error(err));
 });
