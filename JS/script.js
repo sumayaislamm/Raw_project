01c00e2440c100b964aed67407783223f10aef09
Sumaya <sheikhsumaya622@gmail.com>
2025-11-07 23:50:13 +0600

    after wanna text

--- JS/script.js
+++ JS/script.js
@@ -22,14 +22,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('cards-container');
 
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  fetch('https://jsonplaceholder.typicode.com/posts?_limit=12')
     .then(res => res.json())
     .then(data => {
       data.forEach((post, index) => {
         const card = document.createElement('div');
         card.className = 'card';
         card.style.animationDelay = `${index * 0.2}s`; // adds delay per card
-        card.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+        card.innerHTML = 
+        `<h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <button class="card-btn">Read More</button>`;
         container.appendChild(card);
       });
     })
