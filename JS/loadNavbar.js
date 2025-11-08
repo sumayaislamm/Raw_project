34a257a6061aadaf5f20eb53d3d7fc8c44eab11b
Sumaya <sheikhsumaya622@gmail.com>
2025-11-09 01:09:18 +0600

    that's all for today

--- JS/loadNavbar.js
+++ JS/loadNavbar.js
@@ -1,11 +1,9 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch("navbar.html")
     .then(res => res.text())
     .then(data => {
-      document.getElementById("navbar").innerHTML = data;
+      document.querySelector("header").innerHTML = data;
     })
     .catch(err => console.error("Error loading navbar:", err));
 });
-
-
-
