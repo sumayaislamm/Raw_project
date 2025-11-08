78c1659f6c7c44df8cccd47e284528c612b5ece9
Sumaya <sheikhsumaya622@gmail.com>
2025-11-08 23:39:27 +0600

    page redegined

---
+++ JS/navbar.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(err => console.error("Error loading navbar:", err));
+});
+
+
