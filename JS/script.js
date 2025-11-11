54642836721ea35e781a39dd3cece35ec8304d69
Sumaya <sheikhsumaya622@gmail.com>
2025-11-11 20:44:41 +0600

    after adding brands logo

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,10 @@
 
 
-//For the home page Second section
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const container = document.getElementById('cards-container');
-
-// fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-//   .then(res => res.json())
-//   .then(data => {
-//     data.forEach(post => {
-//       const card = document.createElement('div');
-//       card.className = 'card';
-//       card.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
-//       container.appendChild(card); 
-//     });
-//   })
-//   .catch(err => console.error(err));
-// });
-
 
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('cards-container');
+  const container = document.querySelector('.cards-container');
 
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=12')
+  fetch('https://jsonplaceholder.typicode.com/posts?_limit=8')
     .then(res => res.json())
     .then(data => {
       data.forEach((post, index) => {
