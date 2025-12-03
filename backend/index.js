7d485b698172dfc6bd5917ae02de21ceae9cfcdb
Sumaya <sheikhsumaya622@gmail.com>
2025-12-03 20:31:17 +0600

    recover branches

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config(); // Load .env
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://sheikhsumaya622_db_user:1mwrxLCTgW2zKMK1@cluster1.jsrwa3p.mongodb.net/flowerDB')
+// Connect to MongoDB securely
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
 
 // Flower schema
 const flowerSchema = new mongoose.Schema({
